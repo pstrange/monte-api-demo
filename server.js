7d73a9251d92a0c385fab012905d14ada5635236
iCortes <israel.cortes@econocom.com>
2024-01-07 21:44:15 -0600

    setup instances and scaleng with free cuota

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require("./database")
 const { adminAuth, userAuth } = require("./middleware/auth.js");
 const cookieParser = require("cookie-parser")
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cookieParser())
